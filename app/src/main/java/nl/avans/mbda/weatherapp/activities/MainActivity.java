5d32c7db862ae33cf5817ef312f4f60c6de5f057
Sander Jochems <sanderjochems@hotmail.nl>
2022-02-16 12:01:37 +0100

    Create MainActivity

---
+++ app/src/main/java/nl/avans/mbda/weatherapp/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package nl.avans.mbda.weatherapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import nl.avans.mbda.weatherapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
