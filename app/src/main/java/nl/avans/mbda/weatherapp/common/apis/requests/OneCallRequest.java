5191e2356066d8ef75c173c57b079be72e44e07e
Sander Jochems <sanderjochems@hotmail.nl>
2022-02-21 12:58:09 +0100

    Use JsonRequest in OneCallRequest

--- app/src/main/java/nl/avans/mbda/weatherapp/common/apis/requests/OneCallRequest.java
+++ app/src/main/java/nl/avans/mbda/weatherapp/common/apis/requests/OneCallRequest.java
@@ -2,39 +2,12 @@ package nl.avans.mbda.weatherapp.common.apis.requests;
 
 import androidx.annotation.Nullable;
 
-import com.android.volley.NetworkResponse;
-import com.android.volley.ParseError;
-import com.android.volley.Request;
 import com.android.volley.Response;
-import com.android.volley.toolbox.HttpHeaderParser;
 
-import java.io.IOException;
-
-import nl.avans.mbda.weatherapp.models.Converter;
 import nl.avans.mbda.weatherapp.models.onecall.OneCall;
 
-public class OneCallRequest extends Request<OneCall> {
-
-    private final Response.Listener<OneCall> mListener;
-
+public class OneCallRequest extends JsonRequest<OneCall> {
     public OneCallRequest(String url, Response.Listener<OneCall> listener, @Nullable Response.ErrorListener errorListener) {
-        super(Method.GET, url, errorListener);
-        mListener = listener;
-    }
-
-    @Override
-    protected Response<OneCall> parseNetworkResponse(NetworkResponse response) {
-        try {
-            String json = new String(response.data, HttpHeaderParser.parseCharset(response.headers));
-
-            return Response.success(Converter.fromJsonString(json, OneCall.class), HttpHeaderParser.parseCacheHeaders(response));
-        } catch (IOException e) {
-            return Response.error(new ParseError(e));
-        }
-    }
-
-    @Override
-    protected void deliverResponse(OneCall response) {
-        mListener.onResponse(response);
+        super(url, listener, errorListener, OneCall.class);
     }
 }
