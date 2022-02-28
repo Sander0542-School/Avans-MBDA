c7b1f8fc81753106fb8a358a175cb771bfdbccf6
Tommy den Reijer <30556476+tommydr@users.noreply.github.com>
2022-02-28 15:43:47 +0100

    added wallpaper select

--- app/src/main/java/nl/avans/mbda/weatherapp/fragments/DetailedWeatherFragment.java
+++ app/src/main/java/nl/avans/mbda/weatherapp/fragments/DetailedWeatherFragment.java
@@ -1,29 +1,36 @@
 package nl.avans.mbda.weatherapp.fragments;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.preference.PreferenceManager;
-
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.preference.PreferenceManager;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
 import nl.avans.mbda.weatherapp.R;
-import nl.avans.mbda.weatherapp.adapters.DailyWeatherAdapter;
 import nl.avans.mbda.weatherapp.common.Formats;
 import nl.avans.mbda.weatherapp.databinding.FragmentDetailedWeatherBinding;
 import nl.avans.mbda.weatherapp.models.onecall.Daily;
@@ -35,6 +42,31 @@ public class DetailedWeatherFragment extends Fragment {
     private FragmentDetailedWeatherBinding binding;
     private Daily daily;
     private SharedPreferences preferences;
+
+    private ActivityResultLauncher<String> permissionLauncher;
+    private ActivityResultLauncher<String> imagePickerLauncher;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+            if (isGranted) {
+                imagePickerLauncher.launch("image/*");
+            }
+        });
+
+        imagePickerLauncher = registerForActivityResult(new ActivityResultContracts.GetContent(), uri -> {
+            Drawable yourDrawable;
+            try {
+                InputStream inputStream = tryGetContentResolver(getContext()).openInputStream(uri);
+                yourDrawable = Drawable.createFromStream(inputStream, uri.toString());
+                binding.detailScreen.setBackground(yourDrawable);
+            } catch (FileNotFoundException e) {
+                binding.detailScreen.setBackgroundColor(0x00000000);
+            }
+        });
+    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -60,18 +92,29 @@ public class DetailedWeatherFragment extends Fragment {
             binding.minMaxTextView.setText(String.format(locale, Formats.FORMAT_MIN_MAX_TEMP, daily.getTemp().getMin(), daily.getTemp().getMax()));
 
             Button button = binding.button;
-            button.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    String lat  = preferences.getString("latitude", null);
-                    String lon  = preferences.getString("longitude", null);
-                    String uri = String.format("google.streetview:cbll=%s,%s", lat, lon);
-                    Uri gmmIntentUri = Uri.parse(uri);
-                    Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-                    mapIntent.setPackage("com.google.android.apps.maps");
-                    startActivity(mapIntent);
+            button.setOnClickListener(v -> {
+                String lat = preferences.getString("latitude", null);
+                String lon = preferences.getString("longitude", null);
+                String uri = String.format("google.streetview:cbll=%s,%s", lat, lon);
+                Uri gmmIntentUri = Uri.parse(uri);
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+            });
+
+            Button buttonWallpaper = binding.buttonWallpaper;
+            buttonWallpaper.setOnClickListener(v -> {
+                if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
+                    return;
                 }
+                imagePickerLauncher.launch("image/*");
             });
         });
+    }
 
+    static public ContentResolver tryGetContentResolver(Context c) {
+        return c.getContentResolver();
     }
 }
+
